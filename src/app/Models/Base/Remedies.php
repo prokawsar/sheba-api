82020dec9f2aee55a5f05f8146f998e2f360737b
prokawsar <prokawsar@gmail.com>
2020-03-18 08:42:57 +0600

    remedies table added

---
+++ src/app/Models/Base/Remedies.php
@@ -0,0 +1,26 @@
+<?php
+namespace Models\Base;
+
+class Remedies extends \Models\Base
+{
+    protected $fieldConf = [
+        'name' => [
+            'type' => 'VARCHAR128',
+            'nullable' => true
+        ],
+        'deleted' => [
+            'type' => 'INT1',
+            'nullable' => true
+        ],
+        'created' => [
+            'type' => 'DATETIME',
+            'nullable' => true
+        ],
+        'modified' => [
+            'type' => 'DATETIME',
+            'nullable' => true
+        ]
+    ],
+    $table = 'remedies';
+
+}
